2ea94b52c0d51fde5b2e4695de38a82d665050bf
Haunted Bees <fench@hauntedbees.com>
2024-10-30 00:17:58 -0700

    getting better

---
+++ example_scenes/font_size.gd
@@ -0,0 +1,10 @@
+extends ExampleSceneRoot
+
+func _on_scale_2_pressed() -> void:
+	GASText.override_font_scale = 1.5
+
+func _on_scale_15_pressed() -> void:
+	GASText.override_font_scale = 1.25
+
+func _on_scale_1_pressed() -> void:
+	GASText.override_font_scale = 1.0
