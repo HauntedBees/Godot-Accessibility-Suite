72dafc490ce99df1b061a8133b0ffe850af65a93
Haunted Bees <fench@hauntedbees.com>
2024-11-02 00:35:45 -0700

    homepage cleanup

--- example_scenes/roots/home_page.gd
+++ example_scenes/roots/home_page.gd
@@ -2,3 +2,12 @@ extends ExampleSceneRoot
 
 func _on_accessible_rich_text_label_meta_clicked(meta: Variant) -> void:
 	OS.shell_open(str(meta))
+
+func _on_font_reg_pressed() -> void:
+	GASText.override_font_scale = 1.0
+
+func _on_font_big_pressed() -> void:
+	GASText.override_font_scale = 1.1
+
+func _on_font_bigger_pressed() -> void:
+	GASText.override_font_scale = 1.25
